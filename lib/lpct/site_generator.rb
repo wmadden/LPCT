3460c0f2eec31714d11e2efcba859373bce094fd
Will Madden <will.madden@playup.com>
2011-10-12 00:17:14 +1100

    Implemented site generator.

--- lib/lpct/site_generator.rb
+++ lib/lpct/site_generator.rb
@@ -1,8 +1,72 @@
 require "lpct/models"
+require "nokogiri"
+require "erb"
 
 class LPCT::SiteGenerator
   
   attr_accessor :destinations
+  attr_accessor :taxonomy
+  
+  def initialize( destination_template, taxonomy )
+    @template = ERB.new( File.read(destination_template) )
+    @taxonomy = taxonomy
+  end
+  
+  def generate( destinations_file )
+    
+    # A Nokogiri Reader reads an XML file from a stream, so we don't need to load the
+    # entire thing into memory.
+    reader = Nokogiri::XML::Reader.from_io( File.open(destinations_file) )
+    
+    reader.each do |node|
+      next unless node.name == "destination"
+      
+      destination = parse_destination_xml( node.outer_xml )
+      generate_destination_html_for destination
+      
+      # Skip everything until the closing tag
+      reader.read until node.name == "destination" && node.node_type == Nokogiri::XML::Reader::TYPE_END_ELEMENT
+    end
+    
+  end
+  
+  def generate_destination_html_for( destination )
+    destination_taxonomy_node = @taxonomy.nodes[ destination[:id] ]
+    ancestors = @taxonomy.ancestors_of( destination_taxonomy_node ).reverse
+    children = destination_taxonomy_node[:children]
+    
+    file = File.open( "out/#{ destination[:id] }.html", 'w' )
+    file.write( @template.result(binding) )
+  end
+  
+  # Normalizes the destination object, so the view doesn't have to worry about missing properties etc.
+  def parse_destination_xml( xml_string )
+    xml = Nokogiri::XML( xml_string )
+    {
+      :id           => xml.root["atlas_id"],
+      :title        => xml.root["title"],
+      :introduction => {
+        :overview   => xml.root.xpath("introductory/introduction/overview")
+      },
+      :history      => {
+        :overview   => xml.root.xpath("history/history/overview"),
+        :paras      => xml.root.xpath("history/history/history")
+      },
+      :practical    => {
+        :health_and_safety => {
+          # :dangers_and_annoyances => xml.root.xpath("history/history/overview"),
+          #           :while_youre_there => xml.root.xpath("history/history/overview"),
+        },
+        :money_and_costs => {
+          # :money => ,
+          # :costs => 
+        },
+        :visas => {
+          # :overview => 
+        }
+      },
+    }
+  end
   
   def parse_xml( destinations_doc, taxonomy_doc )
     @destinations = {}
--- spec/lib/lpct/site_generator_spec.rb
+++ spec/lib/lpct/site_generator_spec.rb
@@ -2,31 +2,59 @@ require "lpct"
 
 describe( LPCT::SiteGenerator ) do
   
-  let(:destinations_doc) do
-    Nokogiri::XML %(
-      <destinations>
-        <destination atlas_id="355064">
-          Some Destination
-        </destination>
-        <destination atlas_id="123456">
-          Some Other Destination
-        </destination>
-      <destinations>
-    )
-  end
-  let(:taxonomy_doc)     { stub(:taxonomy_doc) }
-  let(:generator)        { LPCT::SiteGenerator.new }
+  let(:taxonomy)  { mock(LPCT::Models::Taxonomy) }
+  let(:generator) { LPCT::SiteGenerator.new("destination_template", taxonomy) }
+  let(:template)  { stub(:erb_template) }
   
-  describe("#parse_xml") do
+  before do
+    File.stub!(:read)
+    ERB.stub!(:new).and_return( template )
+  end
+  
+  describe("#generate") do
     
-    subject { generator.parse_xml( destinations_doc, taxonomy_doc ) }
+    let(:reader) do
+      Nokogiri::XML::Reader.from_memory( %(
+        <destinations>
+          <destination atlas_id="355064">
+            Some Destination
+          </destination>
+          <destination atlas_id="123456">
+            Some Other Destination
+          </destination>
+        </destinations>
+      ))
+    end
+    let(:destinations_file) { "some_file.xml" }
     
-    it "should create a hash of destination IDs to destination objects" do
+    subject { generator.generate( destinations_file ) }
+    
+    before do
+      generator.stub!(:parse_destination_xml)
+      generator.stub!(:generate_destination_html_for)
+      File.stub!(:open)
+      Nokogiri::XML::Reader.stub!(:from_io).and_return( reader )
+    end
+    
+    it "should read the destinations XML using Nokogiri" do
+      file_descriptor = stub( :file_descriptor )
+      File.should_receive(:open).with( destinations_file ).and_return( file_descriptor )
+      Nokogiri::XML::Reader.should_receive(:from_io).with( file_descriptor ).and_return( reader )
       subject
-      generator.destinations["355064"].xml.content.strip.should == "Some Destination"
-      generator.destinations["123456"].xml.content.strip.should == "Some Other Destination"
+    end
+    
+    it "should parse each destination" do
+      generator.should_receive(:parse_destination_xml).exactly(2)
+      subject
+    end
+    
+    it "should generate html for each destination" do
+      generator.should_receive(:generate_destination_html_for).exactly(2)
+      subject
     end
     
   end
+  
+  describe("#")
   
 end
